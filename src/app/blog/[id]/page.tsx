97579eb25e73421b13ec1bc59d357a6a57acd900
Kavindu Harshana <45476434+harshanacz@users.noreply.github.com>
2024-11-21 20:25:38 +0530

    build error fixed

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -7,6 +7,7 @@ import 'react-loading-skeleton/dist/skeleton.css';  // Import styles for the ske
 import BlogPageHeader from '@/components/BlogPage/BlogPageHeader';
 import { StaticImageData } from 'next/image';
 import Footer from '@/components/Footer';
+import Image from 'next/image';
 
 interface BlogPost {
   id: number;
@@ -44,11 +45,12 @@ const BlogPostPage = () => {
       {data ? (
         <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
           <div className="mb-4">
-            <img
-              src={data.image.src}
+            <Image
+              src={data.image}
               alt={data.title}
               className="w-full h-72 object-cover rounded-lg"
             />
+            
           </div>
           <div className="text-gray-600 text-sm flex justify-between items-center mb-4">
             <span className="px-3 py-1 bg-blue-100 text-blue-700 rounded-full">{data.category}</span>
@@ -57,11 +59,12 @@ const BlogPostPage = () => {
           <h1 className="text-3xl font-semibold text-gray-900 mb-4">{data.title}</h1>
           <div className="text-gray-700 mb-6">{data.description}</div>
           <div className="flex items-center">
-            <img
-              src={data.author_img.src}
+            <Image 
+              src={data.author_img}
               alt={data.author}
               className="w-12 h-12 rounded-full mr-4"
             />
+           
             <div>
               <p className="text-gray-900 font-semibold">{data.author}</p>
               <p className="text-gray-600 text-sm">Author</p>
--- src/components/HomePage/BlogPostsSection/BlogCard.tsx
+++ src/components/HomePage/BlogPostsSection/BlogCard.tsx
@@ -1,43 +1,43 @@
-import React from 'react'
-import { StaticImageData } from 'next/image';
-import Link from 'next/link';
+import React from "react";
+import { StaticImageData } from "next/image";
+import Link from "next/link";
+import Image from "next/image";
 
 interface BlogCardProps {
-    title: string;
-    description: string;
-    image: StaticImageData;
-    category: string;
-    id:any;
-  }
-  
-  const BlogCard: React.FC<BlogCardProps> = ({
-    title,
-    description,
-    image,
-    category,
-    id
-  }) => {
-  return (
-    <div className="max-w-[330px] sm:max-w-[300px] bg-blue-50 rounded-lg">
-     <Link href={`/blog/${id}`}>
-
- 
-      <img src={image.src} alt={title} className="w-full h-48 object-cover rounded-md" />
-      <div className="mx-4 my-4">
-        <p className="font-semibold text-black text-base bg-blue-200 inline px-2 py-1 rounded-lg">
-          {category}
-        </p>
-
-        <p className="font-semibold text-black text-lg mt-2">
-          {title}
-        </p>
-
-        <p className="font-normal text-black text-lg mt-2">
-          {description}
-        </p>
-      </div>     </Link>
-    </div>
-  )
+  title: string;
+  description: string;
+  image: StaticImageData;
+  category: string;
+  id: any;
 }
 
-export default BlogCard
+const BlogCard: React.FC<BlogCardProps> = ({
+  title,
+  description,
+  image,
+  category,
+  id,
+}) => {
+  return (
+    <div className="max-w-[330px] sm:max-w-[300px] bg-blue-50 rounded-lg">
+      <Link href={`/blog/${id}`}>
+        <Image
+          src={image}
+          alt={title}
+          className="w-full h-48 object-cover rounded-md"
+        />
+        <div className="mx-4 my-4">
+          <p className="font-semibold text-black text-base bg-blue-200 inline px-2 py-1 rounded-lg">
+            {category}
+          </p>
+
+          <p className="font-semibold text-black text-lg mt-2">{title}</p>
+
+          <p className="font-normal text-black text-lg mt-2">{description}</p>
+        </div>{" "}
+      </Link>
+    </div>
+  );
+};
+
+export default BlogCard;
